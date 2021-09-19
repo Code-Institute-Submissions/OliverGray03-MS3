d5a8f56b8b1b236ba941b769f6d3b57e5d12856b
OliverGray03 <Oliver.gray90@outlook.com>
2021-09-19 18:37:22 +0000

    Completed save recipe function and non logged in users now recieve a flash and redirect to register page when trying to view full recipe

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import random
 from flask import (
     Flask, flash, render_template,
-    redirect, request, session, url_for)
+    redirect, request, session, url_for, abort)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -50,14 +50,19 @@ def get_recipe():
             dbSearch["category_name"] = category_search
 
         recipes = list(mongo.db.recipe_detail.find(dbSearch))
-        categories = mongo.db.categories.find().sort("category_name", 1 )
+        categories = mongo.db.categories.find().sort("category_name", 1)
 
         return render_template(
-        "get_recipe.html", recipes=recipes, categories=categories)
+            "get_recipe.html",
+            recipes=recipes,
+            categories=categories)
 
     recipes = mongo.db.recipe_detail.find()
-    categories = mongo.db.categories.find().sort("category_name", 1 )
-    return render_template("get_recipe.html", recipes=recipes, categories=categories)
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template(
+        "get_recipe.html",
+        recipes=recipes,
+        categories=categories)
 
 
 @app.route("/add_recipe", methods=["GET", "POST"])
@@ -83,23 +88,24 @@ def add_recipe():
         flash("Recipe Successfully Added")
         return redirect(url_for("get_recipe"))
     
-        
-    categories = mongo.db.categories.find().sort("category_name", 1 )
-    difficulty = mongo.db.difficulty.find().sort("difficulty", 1 )
-    return render_template("add_recipe.html", categories=categories, difficulty=difficulty)
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    difficulty = mongo.db.difficulty.find().sort("difficulty", 1)
+    return render_template(
+        "add_recipe.html",
+        categories=categories,
+        difficulty=difficulty)
 
 
 @app.route("/full_recipe/<recipe_id>")
 def full_recipe(recipe_id):
-    #if ()#user session doesnt exist)
-        #flash("Your profile has been succesfully deleted")
-        #redirect to register
+    if "user" not in session:
+        flash("You must register to be able to view a recipe")
+        return redirect(url_for("register"))
+        
+    recipe = mongo.db.recipe_detail.find_one({"_id": ObjectId(recipe_id)})
 
-    recipe = mongo.db.recipe_detail.find_one({"_id" :ObjectId(recipe_id)})
-
-    #404 page doesnt exist
-    #if (!recipe):
-        #return #redirect to 404
+    if recipe is None:
+        abort(404)
 
     if session["user"]:
         return render_template(
@@ -107,7 +113,7 @@ def full_recipe(recipe_id):
             recipe=recipe
         )
     else:
-        flash("Please login to veiw the full recipe") 
+        flash("Please login to veiw the full recipe")
         return render_template(url_for("register"))
 
 
@@ -130,6 +136,7 @@ def register():
             "firstname": request.form.get("firstname").lower(),
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password")),
+            "saved_recipes": [],
         }
         mongo.db.users.insert_one(register)
 
@@ -180,16 +187,27 @@ def logout():
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
-    
-    recipes = list(mongo.db.recipe_detail.find({"created_by": session['user']}))
+    currentUser = mongo.db.users.find_one(
+        {"username": session["user"]})
+    username = currentUser["username"]
+   
+    recipes = list(mongo.db.recipe_detail.find(
+        {"created_by": session['user']}))
+
+    saved_recipes = []
+
+    if "saved_recipes" in currentUser:
+        for recipe_id in currentUser["saved_recipes"]:
+            recipe = mongo.db.recipe_detail.find_one(
+                {"_id":ObjectId(recipe_id)})
+            saved_recipes.append(recipe)
 
     if session["user"]:
         return render_template(
             "profile.html",
             username=username,
-            recipes=recipes)
+            recipes=recipes,
+            saved_recipes=saved_recipes)
 
     return redirect(url_for("login"))
 
@@ -209,8 +227,15 @@ def remove_profile():
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
     recipe = mongo.db.recipe_detail.find_one({"_id": ObjectId(recipe_id)})
+
     created_by = recipe['created_by']
+
     if session["user"] == "admin" or created_by == session["user"]:
+
+        for recipes in saved_recipes:
+            mongo.db.users.update_many(
+                recipes, {"$pull": {"saved_recipes": ObjectId(recipe_id)}})
+
         mongo.db.recipe_detail.delete_one(recipe)
         flash("Recipe Successfully Deleted")
         return redirect(url_for("profile", username=session['user']))
@@ -221,7 +246,6 @@ def edit_recipe(recipe_id):
 
     recipe = mongo.db.recipe_detail.find_one({"_id": ObjectId(recipe_id)})
     created_by = recipe['created_by']
-
 
     if request.method == "POST":
         gf_free = "on" if request.form.get("gf_free") else "off"
@@ -240,25 +264,51 @@ def edit_recipe(recipe_id):
             "cuisine": request.form.get("cuisine")
         }
 
-
         mongo.db.recipe_detail.update({"_id": ObjectId(recipe_id)}, edit)
         flash("Recipe Successfully Updated")
         return redirect(url_for("home"))
 
-
     categories = mongo.db.categories.find().sort("category_name", 1)
-    difficulty = mongo.db.difficulty.find().sort("difficulty", 1 )
+    difficulty = mongo.db.difficulty.find().sort("difficulty", 1)
  
     if session["user"] == "admin" or created_by == session["user"]:
         return render_template(
-            "edit_recipe.html", recipe=recipe, categories=categories, difficulty=difficulty)
+            "edit_recipe.html",
+            recipe=recipe,
+            categories=categories,
+            difficulty=difficulty)
+
+
+@app.route("/save/<recipe_id>", methods=["POST"])
+def save_recipe(recipe_id):
+    user = mongo.db.users.find_one({"username": session["user"]})
+    
+
+    if "saved_recipes" in user:
+        if recipe_id in user["saved_recipes"]:
+            flash("Recipe already saved")
+            return redirect(url_for("profile", username=session['user']))
+    
+    mongo.db.users.update_one(
+        user, {"$push": {"saved_recipes": recipe_id}})
+    flash("Recipe saved to profile")
+    return redirect(url_for("profile", username=session['user']))
+
+
+@app.route("/remove/<recipe_id>", methods=["GET", "POST"])
+def delete_saved_recipe(recipe_id):
+    
+    username = mongo.db.users.find_one({"username": session["user"]})
+
+    mongo.db.users.update_one(
+        username, {"$pull": {"saved_recipes": (recipe_id)}})
+    flash("Recipe Removed from Profile")
+    return redirect(url_for("profile", username=session['user']))
 
 
 @app.errorhandler(404)
 def not_found(e):
-  
-# defining function
-  return render_template("404.html")
+    return (render_template("404.html"), 404)
 
 
 if __name__ == "__main__":
